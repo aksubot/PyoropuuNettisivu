7faa8fe06a0448364b51d8809f81c2cb29707e7f
aksubot <->
2025-01-25 22:41:06 +0200

    updated js and cleared html files

--- Tuotteet/tuotteet.js
+++ Tuotteet/tuotteet.js
@@ -34,55 +34,37 @@ hiddenElements.forEach((el) => observer.observe(el));
 
 
 
-const hinnatButton = document.querySelectorAll(".Hdropdown");
-const ktButton = document.querySelectorAll(".Kdropdown");
+const KButton = document.querySelectorAll(".Kdropdown, .Hdropdown");
 
-const hinnat = document.querySelectorAll(".hinnatCont3, .hinnatContSwitch3, .hinnatContNoSwitch2, .hinnatCont2, .hinnatContNoSwitch22");
-const kt = document.querySelectorAll(".ktCont");
+
+const containers = document.querySelectorAll(".hinnatCont3, .hinnatContSwitch3, .hinnatContNoSwitch2, .hinnatCont2, .hinnatContNoSwitch22, .ktCont");
+
+
 
 
 const triangles = document.querySelectorAll(".triangle");
 
-for(let i = 0; i < hinnatButton.length; i++) hinnatButton[i].onclick = function(){
-    // if(i == 1) {
-    //     hinnat[1].classList.toggle("dropdown-active");
-    //     triangles[1].classList.toggle("flip");
-    //     return;
-    // }
 
-    if(kt[i] == undefined){
-        hinnat[i].classList.toggle("dropdown-active");
+for(let i=0; i< KButton.length; i++) KButton[i].onclick = function(){
 
-        if(triangles[i+i] == undefined){
-            triangles[triangles.length-1].classList.toggle("flip");
-            return;
-        }
-        triangles[i+i].classList.toggle("flip");
-
+    if(i==KButton.length-1) {
+        containers[i].classList.toggle("dropdown-active");
+        triangles[i].classList.toggle("flip");
         return;
     }
 
-    if(kt[i].classList.contains("dropdown-active")){ 
-        kt[i].classList.toggle("dropdown-active");
-        let x = i+1;
-        triangles[x+i].classList.toggle("flip");
-    }   
-    hinnat[i].classList.toggle("dropdown-active");
-    triangles[i+i].classList.toggle("flip");
+    if(KButton[i].classList.contains("Kdropdown") && containers[i-1].classList.contains("dropdown-active")){
+        containers[i-1].classList.toggle("dropdown-active");
+        triangles[i-1].classList.toggle("flip");
+    }
+    else if(KButton[i].classList.contains("Hdropdown") && containers[i+1].classList.contains("dropdown-active")){
+        containers[i+1].classList.toggle("dropdown-active");
+        triangles[i+1].classList.toggle("flip");
+    }
+    
+
+    containers[i].classList.toggle("dropdown-active");
+    triangles[i].classList.toggle("flip");
 }
 
-for(let i = 0; i < ktButton.length; i++) ktButton[i].onclick = function(){
-    // if(i == 1) {
-    //     hinnat[1].classList.toggle("dropdown-active");
-    //     triangles[1].classList.toggle("flip");
-    //     return;
-    // }
-    if(hinnat[i].classList.contains("dropdown-active")){
-        hinnat[i].classList.toggle("dropdown-active");
-        triangles[i+i].classList.toggle("flip");
-    }
-    kt[i].classList.toggle("dropdown-active");
-    let x = i+1;
-    triangles[x+i].classList.toggle("flip");
-}
 
